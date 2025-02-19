6a5702516aa19d5d39d0d041e6fe8718ffc38ef3
Celine Lee <celine.y.lee@gmail.com>
2025-02-19 04:03:35 -0500

    cruxeval include now

--- plot_all.sh
+++ plot_all.sh
@@ -2,3 +2,4 @@
 . plot_mult.sh
 . plot_navigate.sh
 . plot_nested_bool.sh
+. plot_cruxeval.sh
--- plot_cruxeval.sh
+++ plot_cruxeval.sh
@@ -3,7 +3,6 @@ python plot_cruxeval.py --output_folder cruxeval/outputs  \
                --n_buckets 3 \
                --k_min 0 --k_max 120 --num_k_buckets 3 \
                --N_min 0 --N_max 40 --num_N_buckets 3 \
-               --l_min 0 --l_max 15 --num_l_buckets 3  \
                --temperature 0.0 --num_beams 1 --num_gens 1 \
                --models ${ALL_MODELS_PLOTTING} \
                --delete_old
@@ -23,7 +22,6 @@ python plot_cruxeval.py --output_folder cruxeval/outputs  \
                --n_buckets 5 \
                --k_min 0 --k_max 120 --num_k_buckets 3 \
                --N_min 0 --N_max 140 --num_N_buckets 3  \
-               --l_min 0 --l_max 15 --num_l_buckets 3 \
                --temperature 0.0 --num_beams 1 --num_gens 1 \
                --models ${ALL_MODELS_PLOTTING} \
                --delete_old
